213adf86fec3d39eb9e87a6e720aedfcc25bc752
JeremyM45 <jeremyvs1945@gmail.com>
2022-04-08 09:10:55 -0700

    add pizza cost calc based on size

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 function Pizza(size, toppings)  {
   this.size = size
   this.toppings = toppings
-  this.cost = 0
+  this.price = 0
 }
+Pizza.prototype.cost  = function()  {
+  if (this.size === "small")  {
+    this.price += 9.99
+  } else if (this.size === "medium")  {
+    this.price += 12.99
+  } else if (this.size === "large")  {
+    this.price += 15.99
+  } else if (this.size === "X-large") {
+    this.price += 18.99
+  }
+};
